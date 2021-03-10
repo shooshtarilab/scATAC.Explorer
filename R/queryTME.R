b90859b34a864842048c574a203af0e3f836d8b5
Erik Christensen <echris3@uwo.ca>
2021-03-10 10:35:48 -0500

    update queryTME parameters for scATAC metadata

--- R/queryTME.R
+++ R/queryTME.R
@@ -6,16 +6,20 @@
 #' This should only be done on machines with a large amount of ram (>64gb) because some datasets are quite large.
 #' In most cases it is recommended to instead filter databases with some criteria.
 #' @param geo_accession Search by geo accession number. Good for returning individual datasets
-#' @param score_type Search by type of score (TPM, FPKM, raw count)
-#' @param has_signatures Return only those datasets that have cell-type gene signatures available, or only those without (TRUE/FALSE)
-#' @param has_truth Return only those datasets that have cell-type annotations available, or only those without annotations
-#' @param tumour_type Search by type of tumour represented by the dataset
 #' @param author Search by the author who published the dataset
 #' @param journal Search by the journal the dataset was published in.
 #' @param year Search by exact year or year ranges with '<', '>', or '-'. For example, you can return datasets newer than 2013 with '>2013'
 #' @param pmid Search by Pubmed ID associated with the study. Good for returning individual datasets
 #' @param sequence_tech Search by sequencing technology used to sample the cells.
+#' @param score_type Search by type of score (TPM, FPKM, raw count)
+#' @param has_clusters Return only those datasets that have clustering results available, or only those without (TRUE/FALSE)
+#' @param has_truth Return only those datasets that have cell-type annotations available, or only those without annotations
 #' @param organism Search by source organism used in the study, for example human or mouse.
+#' #TODO update docs
+#' @param genome_build
+#' @param category
+#' @param tissue
+#' @param disease
 #' @param metadata_only Return rows of metadata instead of actual datasets. Useful for exploring what data is available without actually downloading data. Defaults to FALSE
 #' @param sparse Return expression as a sparse matrix. 
 #'                  Uses less memory but is less convenient to view, recommended only if encounter memory issues with dense data. Defaults to FALSE.
@@ -40,47 +44,25 @@
 #' res <- queryTME(geo_accession = "GSE72056")
 
 queryTME <- function(geo_accession=NULL,
-                    score_type=NULL,
-                    has_signatures=NULL,
-                    has_truth=NULL,
-                    tumour_type=NULL,
                     author=NULL, 
                     journal=NULL, 
                     year=NULL, 
                     pmid=NULL, 
                     sequence_tech=NULL, 
+                    score_type=NULL,
+                    has_clusters=NULL,
+                    has_truth=NULL,
                     organism=NULL,
+                    genome_build=NULL, #TODO add this code
+                    category=NULL, #TODO add code
+                    tissue=NULL, #TODO add code
+                    disease=NULL, #TODO add code
+
                     metadata_only=FALSE,
                     sparse = FALSE){
     df <- tme_meta
     if (!is.null(geo_accession)) {
         df <- df[df$accession == geo_accession,]
-        #TODO also need to remove it here, assuming it isn't the requested one 
-        #(although i suppose if it isn't asked for it isn't possible to make it past this check)
-    } else {
-        #TODO remove the added example data so it doesn't get included in analysis
-    }
-    if (!is.null(score_type)) {
-        #TODO eventually this will become a way to select which type of score you want to 
-        # download since we will store multiple types
-        df <- df[toupper(df$score_type) == toupper(score_type) ,]
-    }
-    if (!is.null(has_signatures)) {
-        if (has_signatures) {
-            df <- df[df$signatures == 'Y' ,]
-        }else if (!has_signatures) {
-            df <- df[df$signatures == 'N' ,]
-        }
-    }
-    if (!is.null(has_truth)) {
-        if (has_truth) {
-            df <- df[df$cell_labels == 'Y', ]
-        }else if (!has_truth) {
-            df <- df[df$cell_labels == 'N', ]
-        }
-    }
-    if (!is.null(tumour_type)) {
-        df <- df[toupper(df$tumor_type) == toupper(tumour_type),]
     }
     if (!is.null(author)) {
         df <- df[toupper(df$author) == toupper(author),]
@@ -116,11 +98,48 @@ queryTME <- function(geo_accession=NULL,
         df <- df[df$PMID == pmid,]
     }
     if (!is.null(sequence_tech)) {
-        df <- df[toupper(df$Technology) == toupper(sequence_tech),]
+        #TODO
+        df <- df[toupper(df$sequencing_tech) == toupper(sequence_tech),]
+    }
+    if (!is.null(score_type)) {
+        #TODO eventually this will become a way to select which type of score you want to 
+        # download since we will store multiple types
+        df <- df[toupper(df$score_type) == toupper(score_type) ,]
+    }
+    if (!is.null(has_clusters)) {
+        if (has_clusters) {
+            df <- df[df$clusters == 'Y' ,]
+        }else if (!has_clusters) {
+            df <- df[df$clusters == 'N' ,]
+        }
+    }
+    if (!is.null(has_truth)) {
+        if (has_truth) {
+            df <- df[df$cell_labels == 'Y', ]
+        }else if (!has_truth) {
+            df <- df[df$cell_labels == 'N', ]
+        }
     }
     if (!is.null(organism)) {
         df <- df[toupper(df$Organism) == toupper(organism),]
     }
+    if (!is.null(genome_build)) {
+        df <- df[toupper(df$Genome_Build) == toupper(genome_build),]
+    }
+    if (!is.null(category)) {
+        #TODO this column is a list of strings separated by "+", 
+        # need to actually read it as a list
+        df <- df[toupper(df$broad_cell_categories) == toupper(category),]
+    }
+    if (!is.null(tissue)) {
+        #TODO also a list
+        df <- df[toupper(df$tissue_type) == toupper(tissue),]
+    }
+    if (!is.null(disease)) {
+        #TODO also a list
+        df <- df[toupper(df$Disease) == toupper(disease),]
+    }
+
     if (metadata_only) {
         df[,c('signature_link', 'expression_link', 'truth_label_link','sparse_expression_link')] <- list(NULL)
         return(list(df))
