de946f8224ed7087ad49a993152cc21a560abb3b
Arrian Gibson-Khademi <agibsonk@uwo.ca>
2021-10-15 09:42:31 -0400

    added tests for download functions

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(scATAC.Explorer)
+
+test_check("scATAC.Explorer")
---
+++ tests/testthat/testdownloadfuncts.R
@@ -0,0 +1,17 @@
+
+library(scATAC.Explorer)
+
+# should return a list with metadata dataframe inside
+test_that("Test metadata table retrieval", {
+    expect_type(queryATAC(metadata_only = T), "list")
+    expect_s3_class(queryATAC(metadata_only = T)[[1]], "data.frame")
+})
+
+# should be able to download small dataset
+test_that("Test ability to download dataset", {
+    # should retrieve a single dataset
+    expect_equal(length(queryATAC(accession = "GSE89362")), 1)
+    # downloaded dataset should be a SingleCellExperiment object
+    expect_s4_class(queryATAC(accession = "GSE89362")[[1]],
+     "SingleCellExperiment")
+})
